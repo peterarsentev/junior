51d7fa2ad2e2e635699acd568fb0328a7b7e5c2d
Petr Arsentev <parsentev@yandex.ru>
2016-11-11 10:37:05 +0400

    junit

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ * Calculate.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- chapter_001/src/test/java/ru/job4j/package-info.java
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -5,4 +5,4 @@
  * @version $Id$
  * @since 0.1
  */
-package ru.job4j.echo;
+package ru.job4j;
