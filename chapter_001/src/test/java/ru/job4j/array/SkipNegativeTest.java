14886a0f77722a69b2620169f9bee954979025ea
Petr Arsentev <parsentev@yandex.ru>
2020-06-16 07:30:27 +0300

    check reset

--- chapter_001/src/test/java/ru/job4j/array/SkipNegativeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SkipNegativeTest.java
@@ -6,9 +6,22 @@ import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 
 public class SkipNegativeTest {
+    @Test
+    public void whenNotNegative() {
+        int[][] in = {
+                {1, 3},
+                {1, 2}
+        };
+        int[][] expect = {
+                {1, 3},
+                {1, 2}
+        };
+        int[][] rsl = SkipNegative.skip(in);
+        assertThat(rsl, is(expect));
+    }
 
     @Test
-    public void skip() {
+    public void whenHasNegative() {
         int[][] in = {
                 {1, -2},
                 {1, 2}
